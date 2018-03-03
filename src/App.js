6a9bff8979fd9cee026da7eb5e43552d056aa8e9
AlanThinks <AlanThinks@Alans-MacBook-Pro.local>
2018-03-03 17:55:50 -0500

    Update on work on checkbox

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'; //Just cause I saw it somehwere, here I'm importing ONLY Component class from React,
+import React from 'react'; //Just cause I saw it somehwere, here I'm importing ONLY Component class from React,
 import './App.css';           //Importing Styles
 import Title from './title'  //Importing H1 Title from separate file
 
-export default class App extends Component { //I did not use "React.Component" because line 1
-  constructor(){
+export default class App extends React.Component { //I did not use "React.Component" because line 1
+  constructor(nameProps){
+    /*
+    nameProps = {
+    david1: "Morales",
+    alan1: "Guevara"
+    }
+    */
     super();
     this.state={     //giving some initial values to properties of the component
       'inputValue':'',  //Here I'll hold what's being typed as a string
       'taskList': [],  // Here's an array that will hold all the items
+      'davidX': nameProps.david1,
+      'alanX': nameProps.alan1
     }
-
+console.log(this.state.davidX)
 /* Below I binded my two functions so that later in the document
   I can use "this.FUNCTION_NAME" without it casuing an issue */
     this.addToTaskList=this.addToTaskList.bind(this);
@@ -18,9 +26,10 @@ export default class App extends Component { //I did not use "React.Component" b
 
   /*Function below will check IF the key that's pressed is "Enter" (key # 13)
   if so, it will call the function "addToTaskList". */
-  checkForEnterKey(e){
+  checkForEnterKey(eventInfo){
 
-    if (e.which === 13){
+    if (eventInfo.which === 13){
+      this.setState.newTask = eventInfo.target.value
       console.log("'Enter Key' pressed") //Just logging as a test
       this.addToTaskList()   // After checking for the key #13, the function addToTaskList is executed
       }
@@ -37,9 +46,11 @@ export default class App extends Component { //I did not use "React.Component" b
 Below is a function I was using BEFORE, although it worked I changed it because it was
 not the best way.
 
-OnceButtonClicks(e){
+OnceButtonClicks(buttonEventInfo){
   console.log('Button was clicked')
-  this.setState({"inputValue": document.getElementById('task-input').value}, function()
+  let myInput = document.getElementById('task-input')
+
+  this.setState({"newTask": myInput.value}, function()
   {this.addToDo()})
 }
 */
@@ -49,7 +60,10 @@ OnceButtonClicks(e){
     if (this.state.inputValue!==""){            //Will execute only if inputValue is not empty
       let updatingTaskList = this.state.taskList; // Put current array in a temporary variable for readibilty
       updatingTaskList.push(this.state.inputValue) // Here the new task gets pushed into array
-      this.setState({'taskList': updatingTaskList, 'inputValue':""}) //Here I setState w/ new Array and empty the inputValue
+      this.setState({
+        'taskList': updatingTaskList,
+        'inputValue':""
+      }) //Here I setState w/ new Array and empty the inputValue
     } else console.log('Input Blank')           // If the person has not put anything and had pressed enter,
                                                 //nothing will happen
   }
@@ -73,24 +87,24 @@ OnceButtonClicks(e){
                 onKeyDown ={this.checkForEnterKey}
                 type ='text'
                 value = {this.state.inputValue}
-                onChange = {(e)=>
-                  this.setState({'inputValue':e.target.value})}
+                onChange = {(eventInfo)=>
+                  this.setState({'inputValue':eventInfo.target.value})}
                   />
                 {/*<BUTTON>
                   onClick: When the button is clicked it doesn't have to check for checkForEnterKey,
                   so it executes addToTaskList() directly*/}
               <button
                 className="btn-new-task"
-                onClick={this.addToTaskList}
+                onClick={()=>this.addToTaskList()}
                 >Add Task</button>
               {/*<LIST>
                   It's a component I made (see it at the bottom of file)
 
                   Everytime setState is run in any part of document, List gets the updated taskList
                   and pass it as a prop and does stuff with it (see code of Component <List/>)*/}
-              <List
-                listOfTasks = {this.state.taskList}
-                />
+              <List listOfTasks = {this.state.taskList}/>
+
+
             </div>
           </div>
             )
@@ -103,15 +117,21 @@ I bring in the props that were passed in from when it was called and from the su
 I go into the prop object and grab the element with the key "listOfTasks" which was named that way
 above in the render() of App where I assigned it as such: listOfTasks=this.state.taskList (line 94)
 */
-class List extends Component{
+class List extends React.Component{
+  /* props = {
+  listOfTasks: (previous taskList)
+  }
+
+  */
   constructor(props){
     super(props);
     this.state={
-      'listOfTasks': props.listOfTasks,
-      'checkBoxesChecked':{}
+      'listTasks': props.listOfTasks,
+      'checkBoxesChecked':0,
     }
-    //Below I bind the function deleteItem() to this component so that when we use "this.deleteItem" it works without a problem
+    //Below I bind the functions I made to this component so that when we call the function suach as "this.deleteItem" it works without a problem
     this.deleteItem=this.deleteItem.bind(this);
+    this.trackCheckBoxChange=this.trackCheckBoxChange.bind(this);
 
   }
 //Below function is called when delete button is clicked
@@ -119,13 +139,24 @@ deleteItem(e){
   console.log('deleteItem() called') //Just loggint to make sure it was being called
 
   let targetTask = e.target.name     // bringing the name= property of the button into variable targetTask
-  this.state.listOfTasks.splice(targetTask,1)
+  this.state.listTasks.splice(targetTask,1)
   //Above: 'name' of button matches 'id' of <div><li> Task item, so I use that to splice it off array taskList
 
-  this.setState({'listOfTasks':this.state.listOfTasks}) //setting State so it renders with new array and in turn updates list on web app.
+  this.setState({'listTasks':this.state.listTasks}) //setting State so it renders with new array and in turn updates list on web app.
 }
 
-trackCheckBoxChange(){
+//Still working on checkbox tracker
+trackCheckBoxChange(e){
+  console.log('current boxes:',this.state.checkBoxesChecked)
+  let currentBoxesChecked = this.state.checkBoxesChecked
+  console.log("is checkbox checked?",e.target.checked)
+  if (e.target.checked){
+    currentBoxesChecked=+1;
+  } else {
+    currentBoxesChecked=-1
+  }
+  console.log('Current Boxes Checked before setState',currentBoxesChecked)
+  this.setState({'checkBoxesChecked':currentBoxesChecked})
 
 }
 /* Below it renders a UL and then it goes through each item of array listOfTasks and using map
@@ -137,11 +168,11 @@ id will come out like : "0", "1"... etc, I'll use that later to remove items.
 */
     render(){return (
       <ul>
-        {this.state.listOfTasks.map((eachTask,indx,arr)=>
+        {this.state.listTasks.map((eachTask,indx,arr)=>
           (
           <div className ='itemBlue' key={eachTask+indx} id={indx}>
             <li>{eachTask}</li>
-            <input className="chbox" type="checkbox" onChange={this.trackCheckBoxChange}/>
+            <input className="chbox" type="checkbox" onChange={(eventInfo)=>this.trackCheckBoxChange(eventInfo)}/>
 
               {/*Below I add a 'name' property to the button that is the same
               as the id of the above <div> in order to use it, by using the
